08d92803ab70275b6f5fc0bae3021b61e424e4f4
Pablo Martínez López <pmartinezl@full-on-net.com>
2022-05-06 16:36:53 +0200

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Pablo Martinez Lopez on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
